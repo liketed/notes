39b817eb2b5bb2e2d10082c15371f00f3a8b7dca
Steve Kilduff <liketed@users.noreply.github.com>
2014-05-19 22:37:12 +0200

    Add my mdadm.conf to puppet.

--- puppet/etc/modules/common/manifests/init.pp
+++ puppet/etc/modules/common/manifests/init.pp
@@ -1,6 +1,8 @@
 class common {
   # Install needed packages
   class { common::packages :}
+  # Restore mdadm config
+  class { common::mdadm :}
   # 
   #class { common::something :}
 }
---
+++ puppet/etc/modules/common/manifests/mdadm.pp
@@ -0,0 +1,9 @@
+## backup my mdadm config
+class common::mdadm {
+  file { '/etc/mdadm.conf':
+    source  => 'puppet:///modules/common/mdadm/md127.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+}
