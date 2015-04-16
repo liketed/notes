2be28ea63b57c46f96ef0ba5ed85b42bc6c6acc7
liketed <redrocketed@gmail.com>
2015-04-16 17:10:49 +0200

    add some more packages to default setup

--- puppet/etc/modules/common/manifests/packages.pp
+++ puppet/etc/modules/common/manifests/packages.pp
@@ -8,6 +8,7 @@ class common::packages {
 
   $defaultPackages = [
     'bind-utils',
+    'cifs-utils',
     'curl',
     'epel-release',
     'gcc',
@@ -16,6 +17,7 @@ class common::packages {
     'mlocate',
     'rsync',
     'tmux',
+    'telnet',
     'strace',
     'vim-minimal',
     'vim-enhanced',
