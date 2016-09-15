61f02bb1883b5023b01baa2bb2dbf5f982137a7a
liketed <redrocketed@gmail.com>
2016-09-15 18:12:14 +0200

    Update manifest to work with puppet 4 and newer vbox

--- puppet/etc/modules/common/manifests/vagrant.pp
+++ puppet/etc/modules/common/manifests/vagrant.pp
@@ -4,10 +4,11 @@ class common::vagrant {
   exec { 'devtools':
     unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
     command => '/usr/bin/yum -y groupinstall "Development tools"',
-    require => Yumrepo ['virtualbox'],
+    require => Yumrepo['virtualbox'],
   }
   $kernelPackages = [
     'kernel-devel.x86_64',
+    'kernel-core',
     'kernel-headers',
     'mesa-libGL.x86_64',
     'dkms',
@@ -15,25 +16,25 @@ class common::vagrant {
   package { $kernelPackages:,
     ensure        => 'present',
     allow_virtual => false,
-    require       => Exec ['devtools'],
+    require       => Exec['devtools'],
   }
-  package {'VirtualBox-4.3':
+  package {'VirtualBox-5.0':
     ensure  => present,
-    require => Package ['dkms'],
+    require => Package['dkms'],
   }
   exec { 'vboxdrv_setup':
-    command     => '/etc/init.d/vboxdrv setup',
+    command     => '/usr/lib/virtualbox/vboxdrv.sh setup',
     refreshonly => true,
-    subscribe   => Package ['VirtualBox-4.3'],
-    require     => Package ['VirtualBox-4.3'],
+    subscribe   => Package['VirtualBox-5.0'],
+    require     => Package['VirtualBox-5.0'],
   }
   service {'vboxdrv':
     ensure  => running,
-    require => Exec ['vboxdrv_setup'],
+    require => Exec['vboxdrv_setup'],
   }
   exec{'vagrant_install':
     unless  => '/usr/bin/rpm -q vagrant',
-    command => 'rpm -Uvh https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.3_x86_64.rpm',
-    require => Service ['vboxdrv'],
+    command => 'rpm -Uvh https://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.rpm',
+    require => Service['vboxdrv'],
   }
 }
