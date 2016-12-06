15196059c4543f5133c4cf8692c173791dd9a22d
Steve Kilduff <liketed@users.noreply.github.com>
2016-12-06 21:52:37 +0100

    Update to run on fedora 25, with newer versions of paths and tools.

--- micro/install.sh
+++ micro/install.sh
@@ -3,5 +3,5 @@ yum update -y
 yum install -y puppet rubygem-puppet-lint
 yum install -y git vim 
 cd /opt/
-git clone git@github.com:Redrocket/notes.git
-puppet apply --debug --modulepath=/opt/notes/puppet/etc/modules/ --hiera_config=/opt/notes/puppet/etc/manifests/hiera.yaml /opt/notes/puppet/etc/manifests/micro.pp
+git clone git@github.com:liketed/notes.git
+puppet apply --debug --modulepath=/opt/notes/puppet/etc/modules/ --hiera_config=/opt/notes/puppet/etc/manifests/hiera.yaml /opt/notes/puppet/etc/manifests/run.pp
