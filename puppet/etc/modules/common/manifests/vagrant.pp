dd10bc86f809dda31e1efe0938cf4ab6ba17aa1d
Steve Kilduff <liketed@users.noreply.github.com>
2017-03-14 20:54:15 +0100

    fucking Fedora updates annoying the hell out of me. Back to CentOS-7. bump vagrant version.

--- puppet/etc/modules/common/manifests/vagrant.pp
+++ puppet/etc/modules/common/manifests/vagrant.pp
@@ -8,7 +8,6 @@ class common::vagrant {
   }
   $kernelPackages = [
     'kernel-devel.x86_64',
-    'kernel-core',
     'kernel-headers',
     'mesa-libGL.x86_64',
     'dkms',
@@ -34,7 +33,7 @@ class common::vagrant {
   }
   exec{'vagrant_install':
     unless  => '/usr/bin/rpm -q vagrant',
-    command => 'rpm -Uvh https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0_x86_64.rpm',
+    command => 'rpm -Uvh https://releases.hashicorp.com/vagrant/1.9.2/vagrant_1.9.2_x86_64.rpm',
     require => Service['vboxdrv'],
   }
 }
