c2d206bde3c97f711c1610c320a15ba5eada51bb
Steve Kilduff <liketed@users.noreply.github.com>
2017-03-28 23:55:09 +0200

    Add the required jenkins plugins to allow git checkouts.

--- puppet/etc/modules/common/manifests/jenkin.pp
+++ puppet/etc/modules/common/manifests/jenkin.pp
@@ -3,10 +3,32 @@ class common::jenkin {
   class {'jenkins':
   }
 
+  jenkins::plugin { 'display-url-api': }
   jenkins::plugin { 'git': }
-  jenkins::job { 'CentOS-6.8':
-    config => template("${templates}/test-build-job.xml.erb"),
+  jenkins::plugin { 'git-client': }
+  jenkins::plugin { 'junit': }
+  jenkins::plugin { 'mailer': }
+  jenkins::plugin { 'matrix-project': }
+  jenkins::plugin { 'scm-api': }
+  jenkins::plugin { 'script-security': }
+  jenkins::plugin { 'ssh-credentials': }
+  jenkins::plugin { 'structs': }
+  jenkins::plugin { 'workflow-scm-step': }
+  jenkins::plugin { 'workflow-step-api': }
+
+  file {'/var/lib/jenkins/puppet':
+    ensure => directory,
+    owner  => 'jenkins',
+    group  => 'jenkins',
   }
+  vcsrepo { '/var/lib/jenkins/puppet/packer':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/liketed/packer.git',
+  }
+#  jenkins::job { 'CentOS-6.8':
+#    config => template("${templates}/test-build-job.xml.erb"),
+#  }
 
 
 }
