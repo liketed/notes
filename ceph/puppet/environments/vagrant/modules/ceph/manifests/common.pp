c2cd7cd6f9ea1e04745952c875a59b53cbcd0b3b
Steve Kilduff <liketed@users.noreply.github.com>
2017-07-10 22:29:44 +0200

    Install ceph-release rpm on all hosts.

--- ceph/puppet/environments/vagrant/modules/ceph/manifests/common.pp
+++ ceph/puppet/environments/vagrant/modules/ceph/manifests/common.pp
@@ -3,19 +3,24 @@ class ceph::common{
   $subnet = $facts['networking']['interfaces']['eth1']['network'][0,-3]
   package { 'yum-plugin-priorities':
     ensure => present,
-  }
+  }->
+  package {'ceph-release':
+    ensure   => 'present',
+    provider => 'rpm',
+    source   => 'http://download.ceph.com/rpm-jewel/el7/noarch/ceph-release-1-1.el7.noarch.rpm',
+  }->
   package { 'epel-release':
     ensure => present,
     notify => Exec['import-epel-gpg'],
-  }
+  }->
   exec {'import-epel-gpg':
     command     => '/usr/bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
     refreshonly => true,
-  }
+  }->
   service { 'firewalld':
     ensure => stopped,
     enable => false,
-  }
+  }->
   file {'/data':
     ensure => directory,
     owner  => 'ceph',
--- ceph/puppet/environments/vagrant/modules/ceph/manifests/repo.pp
+++
@@ -1,7 +0,0 @@
-class ceph::repo{
-  package {'ceph-release':
-    ensure   => 'present',
-    provider => 'rpm',
-    source   => 'http://download.ceph.com/rpm-jewel/el7/noarch/ceph-release-1-1.el7.noarch.rpm',
-  }
-}
