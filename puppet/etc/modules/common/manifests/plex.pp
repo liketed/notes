cfe67ffabcfbc5d0352a73d6f43f1f8bdcb7b6e3
liketed <redrocketed@gmail.com>
2017-10-02 15:24:31 +0200

    Update plex media server version.

--- puppet/etc/modules/common/manifests/plex.pp
+++ puppet/etc/modules/common/manifests/plex.pp
@@ -3,7 +3,7 @@ class common::plex {
   package { 'plexmediaserver':
     ensure   => 'present',
     provider => 'rpm',
-    source   => 'https://downloads.plex.tv/plex-media-server/1.8.4.4249-3497d6779/plexmediaserver-1.8.4.4249-3497d6779.x86_64.rpm',
+    source   => 'https://downloads.plex.tv/plex-media-server/1.9.2.4285-9f65b88ae/plexmediaserver-1.9.2.4285-9f65b88ae.x86_64.rpm',
   }
   service {'plexmediaserver':
     ensure  => running,
