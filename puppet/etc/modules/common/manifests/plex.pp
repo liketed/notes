e2161c0bde29d044a2249453315b0d956bcd18c7
Steve Kilduff <liketed@users.noreply.github.com>
2018-01-17 21:18:11 +0000

    Update plex rpm version to 1.10.1

--- puppet/etc/modules/common/manifests/plex.pp
+++ puppet/etc/modules/common/manifests/plex.pp
@@ -3,7 +3,7 @@ class common::plex {
   package { 'plexmediaserver':
     ensure   => 'present',
     provider => 'rpm',
-    source   => 'https://downloads.plex.tv/plex-media-server/1.9.2.4285-9f65b88ae/plexmediaserver-1.9.2.4285-9f65b88ae.x86_64.rpm',
+    source   => 'https://downloads.plex.tv/plex-media-server/1.10.1.4602-f54242b6b/plexmediaserver-1.10.1.4602-f54242b6b.x86_64.rpm',
   }
   service {'plexmediaserver':
     ensure  => running,
